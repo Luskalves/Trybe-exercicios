7643cabd61b098a05af6cb0925bd29bdddeb848b
Luskalves <lusca.alves32@gmail.com>
2021-11-26 18:36:11 -0300

    resolvendo exercicios switch case

--- javascript/scripts.js
+++ javascript/scripts.js
@@ -1,18 +1,17 @@
-const currentHour = 22;
+const pessoaCandidata = "aprovada"
 
-if (currentHour >= 22){
-    console.log("Não deveríamos comer nada, é hora de dormir")
-}
+switch(pessoaCandidata){
+    
+    case "aprovada":
+        console.log("Você foi aprovado(a)! :D");
+        break
+    case "lista":
+        console.log("Você esta na lista!");
+        break;
+    case "reprovada":
+        console.log("Você foi reprovada! :(");
+        break;
 
-else if (22 > currentHour && currentHour >= 18){
-    console.log("Rango da noite, vamos jantar :D");
-}
-else if (18 > currentHour && currentHour >= 14){
-    console.log("Vamos fazer um bolo pro café da tarde?");
-}
-else if (14 > currentHour && currentHour >= 11){
-    console.log("Hora do almoço!!!");
-}
-else if (11 > currentHour && currentHour >= 4){
-    console.log("Hmmm, cheiro de café recém passado");
+    default:
+        console.log("Não se aplica!");
 }
