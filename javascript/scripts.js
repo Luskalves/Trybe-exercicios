804ad02af09504c05ca5e43d816527b65034a8c2
Luskalves <lusca.alves32@gmail.com>
2021-11-27 16:47:26 -0300

    exercicio 7

--- javascript/scripts.js
+++ javascript/scripts.js
@@ -1,7 +1,20 @@
-const peçaXadrez = "Rainha";
+const porCento = 100;
 
-
-
-if (peçaXadrez.toLowerCase() === "rainha"){
-    console.log("TODOS - menos o movimento em L do cavalo")
+if(porCento >= 90){
+    console.log("A");
+}
+else if(porCento >= 80){
+    console.log("B");
+}
+else if(porCento >= 70){
+    console.log("C");
+}
+else if(porCento >= 60){
+    console.log("D");
+}
+else if(porCento >= 50){
+    console.log("E");
+}
+else{
+    console.log("F");
 }
