dd735b33481a0ed74147876e325f568de34c4839
Luskalves <lusca.alves32@gmail.com>
2021-11-27 16:41:16 -0300

    exercicio 5

--- javascript/scripts.js
+++ javascript/scripts.js
@@ -1,5 +1,18 @@
-const peçaXadrez = "Rainha";
+let a1 = 10; 
+let a2 = 50;
+let a3 = 120;
 
-if (peçaXadrez === "Rainha"){
-    console.log("TODOS - menos o movimento em L do cavalo")
+const valid = a1 + a2 + a3; 
+
+if(a1 <= 0 || a2 <=0 || a3 <=0 ){
+
+    console.log("ERROR!");
+
+}
+else{
+    if (valid === 180){
+        console.log("True");
+    }else if(valid != 180){
+        console.log("False");
+    }
 }
