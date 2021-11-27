3ecf8bb43d871715b4fdd4b59586dd3bd7d28ffc
Luskalves <lusca.alves32@gmail.com>
2021-11-27 16:51:18 -0300

    exercicio 9

--- javascript/scripts.js
+++ javascript/scripts.js
@@ -1,8 +1,8 @@
-const n1 = 3;
-const n2 = 7;
-const n3 = 6;
+const n1 = 4;
+const n2 = 2;
+const n3 = 9;
 
-if ((n1 % 2) === 0 || (n2 % 2) === 0 || (n3 % 2) === 0){
+if ((n1 % 2) != 0 || (n2 % 2) != 0 || (n3 % 2) != 0){
     
     console.log(true);
 }
