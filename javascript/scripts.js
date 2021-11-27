672685056151b88dc73b68f8548c6b3247d619a8
Luskalves <lusca.alves32@gmail.com>
2021-11-27 15:44:48 -0300

    exercicio 2

--- javascript/scripts.js
+++ javascript/scripts.js
@@ -1,12 +1,8 @@
 let a = 10;
 let b = 5;
 
-soma = a + b;
-subtração = a - b;
-multiplicação = a * b;
-divisão = a / b;
-
-console.log(soma);
-console.log(subtração);
-console.log(multiplicação);
-console.log(divisão);
+if (a > b){
+    console.log(a);
+} else {
+    console.log(b);
+}
