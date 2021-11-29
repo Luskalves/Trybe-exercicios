9cd60de57a663963a4fed30cc6c6c92353a28fcd
Luskalves <lusca.alves32@gmail.com>
2021-11-29 18:16:09 -0300

    exercicio 5 resolvido

--- fundamentos/bloco-4-introducao-a-javascript-e-logica-de-programacao/dia-2-javascript-array-e-loop-for/exercicios.js
+++ fundamentos/bloco-4-introducao-a-javascript-e-logica-de-programacao/dia-2-javascript-array-e-loop-for/exercicios.js
@@ -1,14 +1,11 @@
 let numbers = [5, 9, 3, 19, 70, 8, 100, 2, 35, 27];
-let soma = 0;
+let maiorValor = 0;
 
 for(let i = 0; i < numbers.length; i++){
-    soma += numbers[i];
-}
-let media = soma / 10;
-console.log("a media é de: " + media);
 
-if(media > 20){
-    console.log("O valor é maior que 20");
-}else{
-    console.log("O valor é menor que 20");
+    if(numbers[i] > maiorValor){
+        maiorValor = numbers[i];
+    }    
 }
+
+console.log("O maior valor é o: "+ maiorValor);
