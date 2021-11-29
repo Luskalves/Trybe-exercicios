9f8805e52af1d08e0fbf7d18653c64b1249e869e
Luskalves <lusca.alves32@gmail.com>
2021-11-29 18:19:18 -0300

    exercicio 6 resolvido

--- fundamentos/bloco-4-introducao-a-javascript-e-logica-de-programacao/dia-2-javascript-array-e-loop-for/exercicios.js
+++ fundamentos/bloco-4-introducao-a-javascript-e-logica-de-programacao/dia-2-javascript-array-e-loop-for/exercicios.js
@@ -1,11 +1,11 @@
 let numbers = [5, 9, 3, 19, 70, 8, 100, 2, 35, 27];
-let maiorValor = 0;
+let numerosImpares = 0;
 
 for(let i = 0; i < numbers.length; i++){
-
-    if(numbers[i] > maiorValor){
-        maiorValor = numbers[i];
-    }    
+    
+    if((numbers[i] % 2) != 0){
+        numerosImpares++;
+    }
 }
 
-console.log("O maior valor é o: "+ maiorValor);
+console.log("Existem "+ numerosImpares + " valores impares!");
