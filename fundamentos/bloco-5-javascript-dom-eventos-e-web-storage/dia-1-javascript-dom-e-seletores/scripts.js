f1f39769fbd5e57cd23f91d199021d8ba84423ba
Luskalves <lusca.alves32@gmail.com>
2021-12-06 18:40:50 -0300

    resolvendo exercicios do buscando elementos

---
+++ fundamentos/bloco-5-javascript-dom-eventos-e-web-storage/dia-1-javascript-dom-e-seletores/scripts.js
@@ -0,0 +1,12 @@
+let local = document.getElementById("elementoOndeVoceEsta");
+let localPai = local.parentElement;
+
+console.log(local);
+console.log(local.parentElement);
+console.log(local.firstElementChild);
+console.log(local.firstElementChild.innerText = 'Quero ser Policial Federal!');
+console.log(localPai.childNodes[1]);
+console.log(local.firstElementChild)
+console.log(local.nextSibling);
+console.log(local.nextSibling.nextSibling);
+console.log(localPai.childNodes[5]);
